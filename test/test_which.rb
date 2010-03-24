b68ecec128a0eedbcfb5c2588330310dfeb92047
Daniel Berger <dberger@globe.gov>
2010-03-23 18:04:26 -0600

    Refactored and added several tests for File.which.

--- test/test_which.rb
+++ test/test_which.rb
@@ -16,36 +16,73 @@ require 'ptools'
 include Config
 
 class TC_FileWhich < Test::Unit::TestCase
-   def setup
-      @ruby = RUBY_PLATFORM.match('java') ? 'jruby' : 'ruby'
-      @exe = File.join(CONFIG["bindir"], CONFIG["ruby_install_name"]) 
+  def self.startup
+    @@windows = Config::CONFIG['host_os'] =~ /mswin|msdos|win32|cygwin|mingw/i
+  end
 
-      if Config::CONFIG['host_os'] =~ /mswin|win32|dos|cygwin|mingw/i
-         @exe.tr!('/','\\')
-         @exe << ".exe"
-      end
-   end
+  def setup
+    @ruby = RUBY_PLATFORM.match('java') ? 'jruby' : 'ruby'
+    @exe = File.join(CONFIG['bindir'], CONFIG['ruby_install_name']) 
 
-   def test_which_basic
-      assert_respond_to(File, :which)
-      assert_nothing_raised{ File.which(@ruby) }
-      assert_nothing_raised{ File.which(@ruby, "/usr/bin:/usr/local/bin") }
-   end
+    if @@windows
+      @exe.tr!('/','\\')
+      @exe << ".exe"
+    end
+  end
 
-   def test_which_expected_return_values
-      assert_kind_of(String, File.which(@ruby))
-      assert_equal(@exe, File.which(@ruby))
-      assert_equal(nil, File.which(@ruby, "/bogus/path"))
-      assert_equal(nil, File.which("blahblah"))
-   end
+  test "which basic functionality" do
+    assert_respond_to(File, :which)
+    assert_nothing_raised{ File.which(@ruby) }
+    assert_kind_of(String, File.which(@ruby))
+  end
 
-   def test_which_expected_errors
-      assert_raises(ArgumentError){ File.which }
-      assert_raises(ArgumentError){ File.which(@ruby, "foo", "bar") }
-   end
+  test "which accepts an optional path to search" do
+    assert_nothing_raised{ File.which(@ruby, "/usr/bin:/usr/local/bin") }
+  end
 
-   def teardown
-      @exe  = nil
-      @ruby = nil
-   end
+  test "which returns nil if not found" do
+    assert_equal(nil, File.which(@ruby, '/bogus/path'))
+    assert_equal(nil, File.which('blahblahblah'))
+  end
+
+  test "which handles executables without extensions on windows" do
+    omit_unless(@@windows, "test skipped on MS Windows")
+    assert_not_nil(File.which('ruby'))
+    assert_not_nil(File.which('notepad'))
+  end
+
+  test "which handles executables that already contain extensions on windows" do
+    omit_unless(@@windows, "test skipped on MS Windows")
+    assert_not_nil(File.which('ruby.exe'))
+    assert_not_nil(File.which('notepad.exe'))
+  end
+
+  test "which returns argument if an existent absolute path is provided" do
+    assert_equal(@exe, File.which(@ruby))
+  end
+
+  test "which returns nil if a non-existent absolute path is provided" do
+    assert_equal(nil, File.which('/foo/bar/baz/ruby'))
+  end
+
+  test "which accepts a minimum of one argument" do
+    assert_raises(ArgumentError){ File.which }
+  end
+
+  test "which accepts a maximum of two arguments" do
+    assert_raises(ArgumentError){ File.which(@ruby, "foo", "bar") }
+  end
+
+  test "the second argument cannot be nil" do
+    assert_raises(ArgumentError){ File.which(@ruby, nil) }
+  end
+
+  def teardown
+    @exe  = nil
+    @ruby = nil
+  end
+
+  def self.shutdown
+    @@windows = nil
+  end
 end
