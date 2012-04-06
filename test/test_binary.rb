9bf6b800e82b85b6e0896835dc3120f293e18ab2
Daniel Berger <djberg96@gmail.com>
2012-04-06 10:09:11 -0600

    Refactored the File.binary? tests.

--- test/test_binary.rb
+++ test/test_binary.rb
@@ -1,43 +1,47 @@
 #####################################################################
-# tc_binary.rb
-# 
+# test_binary.rb
+#
 # Test case for the File.binary? method. You should run this test
 # via the 'rake test_binary' task.
 #####################################################################
 require 'rubygems'
-gem 'test-unit'
-
-require 'test/unit'
+require 'test-unit'
 require 'ptools'
 
-class TC_Binary < Test::Unit::TestCase
-   def self.startup
-      Dir.chdir('test') if File.exists?('test')
-      File.open('test_file1.txt', 'w'){ |fh| 10.times{ |n| fh.puts "line #{n}" } }
-   end
-   
-   def setup
-      @text_file = 'test_file1.txt'
-   end
+class TC_Ptools_Binary < Test::Unit::TestCase
+  def self.startup
+    @@txt_file = 'test_binary.txt'
 
-   def test_binary_basic
-      assert_respond_to(File, :binary?)
-      assert_nothing_raised{ File.binary?(@text_file) }
-   end
+    if File::ALT_SEPARATOR
+      @@bin_file = File.join(ENV['windir'], 'notepad.exe')
+    else
+      @@bin_file = '/usr/bin/ls'
+    end
 
-   def test_binary_expected_results
-      assert_equal(false, File.binary?(@text_file))
-   end
+    Dir.chdir('test') if File.exists?('test')
 
-   def test_binary_expected_errors
-      assert_raise_kind_of(SystemCallError){ File.binary?('bogus') }
-   end
+    File.open(@@txt_file, 'w'){ |fh| 10.times{ |n| fh.puts "line #{n}" } }
+  end
 
-   def teardown
-      @text_file = nil
-   end
+  test "File.binary? basic functionality" do
+    assert_respond_to(File, :binary?)
+    assert_nothing_raised{ File.binary?(@@txt_file) }
+  end
 
-   def self.shutdown
-      File.delete('test_file1.txt') if File.exists?('test_file1.txt')
-   end
+  test "File.binary? returns expected results" do
+    assert_false(File.binary?(@@txt_file))
+    assert_true(File.binary?(@@bin_file))
+  end
+
+  test "File.binary? raises an error if the file cannot be found" do
+    assert_raise_kind_of(SystemCallError){ File.binary?('bogus') }
+  end
+
+  test "File.binary? only accepts one argument" do
+    assert_raise_kind_of(ArgumentError){ File.binary?(@@txt_file, @@bin_file) }
+  end
+
+  def self.shutdown
+    File.delete(@@txt_file) if File.exists?(@@txt_file)
+  end
 end
