97f128784f1817b269525e41b66c25e43d788f73
Daniel Berger <djberg96@gmail.com>
2014-02-21 09:30:21 -0700

    Refactored and updated the image tests.

--- test/test_image.rb
+++ test/test_image.rb
@@ -2,7 +2,7 @@
 # test_image.rb
 #
 # Test case for the File.image? method. You should run this test
-# via the 'rake test_image' task.
+# via the 'rake test:image' task.
 #####################################################################
 require 'test-unit'
 require 'ptools'
@@ -15,18 +15,34 @@ class TC_Ptools_Image < Test::Unit::TestCase
 
   def setup
     @text_file = 'test_file1.txt'
+    @jpg_file  = File.join(Dir.pwd, 'img', 'test.jpg')
+    @png_file  = File.join(Dir.pwd, 'img', 'test.png')
+    @gif_file  = File.join(Dir.pwd, 'img', 'test.gif')
   end
 
-  def test_image_basic
+  test "image? method basic functionality" do
     assert_respond_to(File, :image?)
     assert_nothing_raised{ File.image?(@text_file) }
+    assert_boolean(File.image?(@text_file))
   end
 
-  def test_image_expected_results
-    assert_equal(false, File.image?(@text_file))
+  test "image? method returns false for a text file" do
+    assert_false(File.image?(@text_file))
   end
 
-  def test_image_expected_errors
+  test "image? method returns true for a gif" do
+    assert_true(File.image?(@gif_file))
+  end
+
+  test "image? method returns true for a jpeg" do
+    assert_true(File.image?(@jpg_file))
+  end
+
+  test "image? method returns true for a png" do
+    assert_true(File.image?(@png_file))
+  end
+
+  test "image? method raises an error if the file does not exist" do
     assert_raises(Errno::ENOENT, ArgumentError){ File.image?('bogus') }
   end
 
@@ -36,5 +52,8 @@ class TC_Ptools_Image < Test::Unit::TestCase
 
   def self.shutdown
     File.delete('test_file1.txt') if File.exist?('test_file1.txt')
+    @jpeg_file = nil
+    @png_file  = nil
+    @gif_file  = nil
   end
 end
