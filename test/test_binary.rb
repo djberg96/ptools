74cbd15fae53c9d8cf7f89b71177a50fe43c24ed
Daniel Berger <djberg96@gmail.com>
2014-02-21 09:54:32 -0700

    Refactored to test against unicode text and image files.

--- test/test_binary.rb
+++ test/test_binary.rb
@@ -10,8 +10,6 @@ require 'ptools'
 
 class TC_Ptools_Binary < Test::Unit::TestCase
   def self.startup
-    @@txt_file = 'test_binary.txt'
-
     if File::ALT_SEPARATOR
       @@bin_file = File.join(ENV['windir'], 'notepad.exe')
     else
@@ -19,18 +17,34 @@ class TC_Ptools_Binary < Test::Unit::TestCase
     end
 
     Dir.chdir('test') if File.exist?('test')
+  end
 
-    File.open(@@txt_file, 'w'){ |fh| 10.times{ |n| fh.puts "line #{n}" } }
+  def setup
+    @txt_file = File.join('txt', 'english.txt')
+    @uni_file = File.join('txt', 'korean.txt')
+    @png_file = File.join('img', 'test.png')
+    @jpg_file = File.join('img', 'test.jpg')
+    @gif_file = File.join('img', 'test.gif')
   end
 
   test "File.binary? basic functionality" do
     assert_respond_to(File, :binary?)
-    assert_nothing_raised{ File.binary?(@@txt_file) }
+    assert_nothing_raised{ File.binary?(@txt_file) }
   end
 
-  test "File.binary? returns expected results" do
-    assert_false(File.binary?(@@txt_file))
+  test "File.binary? returns true for binary files" do
     assert_true(File.binary?(@@bin_file))
+  end
+
+  test "File.binary? returns false for text files" do
+    assert_false(File.binary?(@txt_file))
+    assert_false(File.binary?(@uni_file))
+  end
+
+  test "File.binary? returns false for image files" do
+    assert_false(File.binary?(@png_file))
+    assert_false(File.binary?(@jpg_file))
+    assert_false(File.binary?(@gif_file))
   end
 
   test "File.binary? raises an error if the file cannot be found" do
@@ -38,10 +52,14 @@ class TC_Ptools_Binary < Test::Unit::TestCase
   end
 
   test "File.binary? only accepts one argument" do
-    assert_raise_kind_of(ArgumentError){ File.binary?(@@txt_file, @@bin_file) }
+    assert_raise_kind_of(ArgumentError){ File.binary?(@txt_file, @@bin_file) }
   end
 
-  def self.shutdown
-    File.delete(@@txt_file) if File.exist?(@@txt_file)
+  def teardown
+    @txt_file = nil
+    @uni_file = nil
+    @png_file = nil
+    @jpg_file = nil
+    @gif_file = nil
   end
 end
