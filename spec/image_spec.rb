7ea78269ab2cf3401954bc8b76eadade9a507ae1
Daniel Berger <djberg96@gmail.com>
2022-11-26 06:56:16 -0500

    Add jpg? specs, add test.bmp file.

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -17,45 +17,68 @@ RSpec.describe File, :image do
     @gif_file  = described_class.join(Dir.pwd, 'img', 'test.gif')
     @ico_file  = described_class.join(Dir.pwd, 'img', 'test.ico')
     @no_ext    = described_class.join(Dir.pwd, 'img', 'jpg_no_ext')
+    @bmp_file  = described_class.join(Dir.pwd, 'img', 'test.bmp')
   end
 
-  example 'image? method basic functionality' do
-    expect(described_class).to respond_to(:image?)
-    expect{ described_class.image?(@txt_file) }.not_to raise_error
-    expect(described_class.image?(@txt_file)).to be(true).or be(false)
+  context 'jpg?' do
+    example 'jpg? method basic functionality' do
+      expect(described_class).to respond_to(:jpg?)
+      expect{ described_class.jpg?(@jpg_file) }.not_to raise_error
+      expect(described_class.jpg?(@jpg_file)).to be(true).or be(false)
+    end
+
+    example 'jpg? method returns true for a jpeg file' do
+      expect(described_class.jpg?(@jpg_file)).to be(true)
+    end
+
+    example 'jpg? method returns true for an image that is not a jpeg' do
+      expect(described_class.jpg?(@gif_file)).to be(false)
+    end
+
+    example 'jpg? method returns true for a text file' do
+      expect(described_class.jpg?(@txt_file)).to be(false)
+    end
   end
 
-  example 'image? method returns false for a text file' do
-    expect(described_class.image?(@txt_file)).to be false
-    expect(described_class.image?(@uni_file)).to be false
-  end
+  context 'image?' do
+    example 'image? method basic functionality' do
+      expect(described_class).to respond_to(:image?)
+      expect{ described_class.image?(@txt_file) }.not_to raise_error
+      expect(described_class.image?(@txt_file)).to be(true).or be(false)
+    end
 
-  example 'image? method returns true for a gif' do
-    expect(described_class.image?(@gif_file)).to be true
-    expect(described_class.image?(@gif_file, check_file_extension: false)).to be true
-  end
+    example 'image? method returns false for a text file' do
+      expect(described_class.image?(@txt_file)).to be false
+      expect(described_class.image?(@uni_file)).to be false
+    end
 
-  example 'image? method returns true for a jpeg' do
-    expect(described_class.image?(@jpg_file)).to be true
-    expect(described_class.image?(@jpg_file, check_file_extension: false)).to be true
-  end
+    example 'image? method returns true for a gif' do
+      expect(described_class.image?(@gif_file)).to be true
+      expect(described_class.image?(@gif_file, check_file_extension: false)).to be true
+    end
 
-  example 'image? method returns true for a png' do
-    expect(described_class.image?(@png_file)).to be true
-    expect(described_class.image?(@png_file, check_file_extension: false)).to be true
-  end
+    example 'image? method returns true for a jpeg' do
+      expect(described_class.image?(@jpg_file)).to be true
+      expect(described_class.image?(@jpg_file, check_file_extension: false)).to be true
+    end
 
-  example 'image? method returns true for an ico' do
-    expect(described_class.image?(@ico_file)).to be true
-    expect(described_class.image?(@ico_file, check_file_extension: false)).to be true
-  end
+    example 'image? method returns true for a png' do
+      expect(described_class.image?(@png_file)).to be true
+      expect(described_class.image?(@png_file, check_file_extension: false)).to be true
+    end
 
-  example 'image? method raises an error if the file does not exist' do
-    expect{ described_class.image?('bogus') }.to raise_error(Exception) # Errno::ENOENT or ArgumentError
-  end
+    example 'image? method returns true for an ico' do
+      expect(described_class.image?(@ico_file)).to be true
+      expect(described_class.image?(@ico_file, check_file_extension: false)).to be true
+    end
 
-  example "image? returns appropriate value if the extension isn't included" do
-    expect(described_class.image?(@no_ext, check_file_extension: true)).to be false
-    expect(described_class.image?(@no_ext, check_file_extension: false)).to be true
+    example 'image? method raises an error if the file does not exist' do
+      expect{ described_class.image?('bogus') }.to raise_error(Exception) # Errno::ENOENT or ArgumentError
+    end
+
+    example "image? returns appropriate value if the extension isn't included" do
+      expect(described_class.image?(@no_ext, check_file_extension: true)).to be false
+      expect(described_class.image?(@no_ext, check_file_extension: false)).to be true
+    end
   end
 end
